3e307da6e5bda75af1b48b1e0675bb3454cb7ded
li-yaoshi <75675514+li-yaoshi@users.noreply.github.com>
2021-01-16 18:06:46 +0800

    Create show_image2.html.py

---
+++ 显示预测图片和结果/show_image2.html.py
@@ -0,0 +1,22 @@
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>predict-show</title>
+</head>
+<body>
+    <div class="container">
+        <br>
+        <h1>predict results: {{dic_res}}</h1> <!--预测结果字典-->
+        <br>
+        <img src="../static/images/out0.jpg" /> <!--图片展示width="500" height="500"-->
+    </div>
+     <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+</html>
+```
